1f4a018f0eeebe791a9c24dcd7a98eaf5081b015
Allen M <tmracing008@yahoo.com>
2019-02-19 18:58:34 -0800

    add route folder and syncOtions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const passport = require("passport");
 
 const db = require("./models");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,12 +11,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(routes);
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("tech-startup/build"));
 }
 
-db.sequelize.sync().then(function() {
+var syncOptions = { force: false };
+
+if(process.env.NODE_ENV === "test") {
+    syncOptions.force = true;
+}
+db.sequelize.sync(syncOptions).then(function() {
     app.listen(PORT, function() {
         console.log(`Listening on port ${PORT}`);
     });
