d1bd2375a0a280c760e0406f586d0906655be46f
Allen M <tmracing008@yahoo.com>
2019-02-26 12:33:37 -0800

    restructed file system, add cookiesession

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const passport = require("passport");
-
-const db = require("./models");
+const cookieSession = require("cookie-session");
 const routes = require("./routes");
+const db = require("./models/");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// cookie sessions middleware
+app.use(
+    cookieSession({
+      maxAge: 24 * 60 * 60 * 1000,
+      keys: ["my secret Key"]
+    })
+  );
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,8 +21,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
 
+require("./routes/api/authRoutes");
+
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static("tech-startup/build"));
+    app.use(express.static("client/build"));
 }
 
 var syncOptions = { force: false };
