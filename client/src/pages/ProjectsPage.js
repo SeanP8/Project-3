a4a02441076ce2887d3f1a392929d13de4db3d4c
KitKat <katherine_nguyen7@yahoo.com>
2019-02-28 10:54:55 -0800

    added projectspage

--- client/src/pages/ProjectsPage.js
+++ client/src/pages/ProjectsPage.js
@@ -3,13 +3,13 @@ import HomeNav from "../components/HomeNav";
 import Wrapper from "../components/Wrapper";
 import Footer from "../components/Footer";
 
-class Home extends Component {
+class Projects extends Component {
     render() {
         return (
             <div>
                 <HomeNav/>
                 <Wrapper>
-                   <p>Home Page!</p> 
+                   <p>Projects Page!</p> 
                 </Wrapper>
                 <Footer />
             </div>
@@ -17,4 +17,4 @@ class Home extends Component {
     }
 }
 
-export default Home;
+export default Projects;
