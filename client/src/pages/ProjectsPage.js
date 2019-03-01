a14dac14255a65d2d133d5c46b63ac548949fe23
KitKat <katherine_nguyen7@yahoo.com>
2019-02-28 17:41:44 -0800

    added title, button, and modal

--- client/src/pages/ProjectsPage.js
+++ client/src/pages/ProjectsPage.js
@@ -7,14 +7,39 @@ class Projects extends Component {
     render() {
         return (
             <div>
-                <HomeNav/>
+                <HomeNav />
                 <Wrapper>
-                   <p>Projects Page!</p> 
-                </Wrapper>
-                <Footer />
-            </div>
-        )
-    }
-}
+                    <div>
+                        <h1 className="subTitle">Projects</h1>
+                    </div>
 
+                    <button onClick={this.handleClick} id="addProject" type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                        + Add Project
+                    </button>
+                    <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div className="modal-dialog modal-dialog-centered" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalCenterTitle">Add Project</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                   FORM GOES HERE 
+                                </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button id="saveProject" type="button" className="btn btn-primary">Save Project</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </Wrapper>
+                    <Footer />
+            </div>
+                )
+            }
+        }
+        
 export default Projects;
