48d429e5dc844f93a72f0e267153f436a39672d0
Phelan McCall <phelanmccall@yahoo.com>
2019-03-02 12:45:10 -0800

    stash

--- models/auths.js
+++ models/auths.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
        
         email: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         },
         avatar: {
             type: DataTypes.STRING,
