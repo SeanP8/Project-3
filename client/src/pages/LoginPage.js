ea63fe483790178db5cfa8a467dc62bcc7c19584
Phelan <phelanmccall@yahoo.com>
2019-03-13 20:23:03 -0700

    better

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ class Login extends React.Component {
   componentWillMount(){
     API.getCurrentUser().then(response => {
       console.log(response.data);
-      if(response.data.email){
+      if(response.data.id){
         window.location.href = "/home"
       }
     })
