9ac58770cabd2ebf5d7df828271e4c9aeb04a7ae
Allen M <tmracing008@yahoo.com>
2019-03-16 14:58:04 -0700

    alter comments

--- client/src/components/CommentBox.js
+++ client/src/components/CommentBox.js
@@ -2,10 +2,11 @@ import React from 'react';
 import CommentForm from './CommentForm';
 
 const CommentBox = props => {
+    const id = props.project
     return (
         <div className="commentBox">
             <h2 className="comment-title">Comments</h2>
-            <CommentForm projectId = {props.project}/>       
+            <CommentForm projectId = {id}/>       
         </div>
     );
 };
