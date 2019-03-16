2a1f6e02963752f13c9a8e66a06e726b15995173
Sean <seanpitfield@gmail.com>
2019-03-16 10:58:17 -0700

    started a favorite button on the allprojects page

--- models/favorite.js
+++ models/favorite.js
@@ -1,41 +1,41 @@
-// module.exports = (sequelize, Datatypes) => {
-// const Favorite = sequelize.define(
-//   "Favorite",
-//   {
-//     favorite: Datatypes.BOOLEAN
-//   },
+module.exports = (sequelize, Datatypes) => {
+  const Favorite = sequelize.define(
+    "Favorite",
+    {
+      favorite: Datatypes.BOOLEAN
+    },
 
-//   {
-//     timestamps: false
-//   }
-// );
-module.exports = (sequelize, DataTypes) => {
-  const Favorite = sequelize.define("Favorite", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 240]
-      }
-    },
-    link: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    image: {
-      type: DataTypes.STRING
-    },
-    projectID: {
-      type: DataTypes.INTEGER
-    },
-    userID: {
-      type: DataTypes.INTEGER,
-      allowNull: true
+    {
+      timestamps: false
     }
-  });
+  );
+  // module.exports = (sequelize, DataTypes) => {
+  //   const Favorite = sequelize.define("Favorite", {
+  //     title: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false,
+  //       validate: {
+  //         len: [1, 240]
+  //       }
+  //     },
+  //     link: {
+  //       type: DataTypes.STRING,
+  //       allowNull: false
+  //     },
+  //     description: {
+  //       type: DataTypes.TEXT,
+  //       allowNull: false
+  //     },
+  //     image: {
+  //       type: DataTypes.STRING
+  //     },
+  //     projectID: {
+  //       type: DataTypes.INTEGER
+  //     },
+  //     userID: {
+  //       type: DataTypes.INTEGER,
+  //       allowNull: true
+  //     }
+  //   });
   return Favorite;
 };
