74fc723226ba2a2d26d8afe51505deefef2e3424
Allen M <tmracing008@yahoo.com>
2019-03-19 18:06:55 -0700

    add canary test

---
+++ test/canary.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+const expect = require("chai").expect;
+
+describe("canary test", function() {
+  it("should pass this canary test", function() {
+    expect(true).to.be.true;
+  });
+});
