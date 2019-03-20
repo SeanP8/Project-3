6718262a884e671fb117043574abd80a20f803a8
Sean <seanpitfield@gmail.com>
2019-03-19 17:29:04 -0700

    deleted comments

--- models/favorite.js
+++ models/favorite.js
@@ -1,12 +1,3 @@
-// module.exports = (sequelize, Datatypes) => {
-//   const Favorite = sequelize.define(
-//     "Favorite",
-//     {
-//       favorite: Datatypes.BOOLEAN
-//     },
-
-//     {
-//       timestamps: false
 module.exports = (sequelize, DataTypes) => {
   const Favorite = sequelize.define("Favorite", {
     projectID: {
@@ -17,33 +8,6 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     }
   });
-  // module.exports = (sequelize, DataTypes) => {
-  //   const Favorite = sequelize.define("Favorite", {
-  //     title: {
-  //       type: DataTypes.STRING,
-  //       allowNull: false,
-  //       validate: {
-  //         len: [1, 240]
-  //       }
-  //     },
-  //     link: {
-  //       type: DataTypes.STRING,
-  //       allowNull: false
-  //     },
-  //     description: {
-  //       type: DataTypes.TEXT,
-  //       allowNull: false
-  //     },
-  //     image: {
-  //       type: DataTypes.STRING
-  //     },
-  //     projectID: {
-  //       type: DataTypes.INTEGER
-  //     },
-  //     userID: {
-  //       type: DataTypes.INTEGER,
-  //       allowNull: true
-  //     }
-  //   });
+
   return Favorite;
 };
